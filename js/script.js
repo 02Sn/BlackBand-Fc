8d56ecf7891fb99cc0c3b15e9ae7684b5e516ceb
02Sn <103293843+02Sn@users.noreply.github.com>
2022-04-08 17:43:19 -0700

    Upload file web
    upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const txtElement = ['Singosari', 'Klampok', 'Gang Apel'];
+let count = 0;
+let textIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++textIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        textIndex = 0;
+    }
+
+    setTimeout(ngetik, 500);
+
+})();
+$('body, html').on('click', function() { $('menu').hide();})
